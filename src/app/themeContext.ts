8508365fefbcbab17cc048980dc0cb871043be5c
Sean Bailey <hello@seanbailey.dev>
2021-03-17 23:26:35 +1100

    Remove old themeContext file

--- src/app/themeContext.ts
+++
@@ -1,9 +0,0 @@
-import React, { useContext } from 'react'
-
-export type Theme = 'light' | 'dark'
-
-export const ThemeContext = React.createContext<Theme>('light')
-
-export function useTheme() {
-  return useContext(ThemeContext)
-}
